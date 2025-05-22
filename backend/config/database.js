eb5951af74abf8b938023912d85f7078c726a555
Thushakaran <thushakarant@gmail.com>
2025-05-22 15:05:10 +0530

    first commit

---
+++ backend/config/database.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_LOCAL_URL).then(con => {
+        console.log(`MongoDB is connected to the host: ${con.connection.host}`)
+    }).catch((err) => {
+        console.log(err);
+    })
+}
+
+module.exports = connectDatabase;
+
+// const mongoose = require('mongoose');
+
+// const connectDatabase = () => {
+//     mongoose.connect(process.env.DB_LOCAL_URL)
+//         .then(con => {
+//             console.log(`MongoDB is connected to the host: ${con.connection.host}`);
+//         })
+//         .catch(err => {
+//             console.error('MongoDB connection error:', err);
+//         });
+// };
+
+// module.exports = connectDatabase;
