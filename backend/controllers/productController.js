24298b2229d97a392a9d64e3572695e9abe5b0de
Thushakaran <thushakarant@gmail.com>
2025-05-23 15:48:37 +0530

    update backend

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,16 @@
+const Product = require('../models/productsModels');
+
 exports.getProducts = (req, res, next) => {
     res.status(200).json({
         success: true,
         messsage: "This route will show all the products in database"
+    })
+}
+
+exports.newProduct = async (req, res, next) => {
+    const product = await Product.create(req.body);
+    res.status(201).json({
+        success: true,
+        product
     })
 }
