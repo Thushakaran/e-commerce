15751e62b8f0276a188f69cd1397f58eef65473d
Thushakaran <thushakarant@gmail.com>
2025-05-24 09:47:34 +0530

    update error handling part

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -1,4 +1,5 @@
 const { stack } = require("../app");
+const ErrorHandler = require("../utils/errorHandler");
 
 module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
@@ -13,9 +14,23 @@ module.exports = (err, req, res, next) => {
     }
 
     if (process.env.NODE_ENV == 'production') {
+        let message = err.message;
+        let error = new Error(message);
+
+        if (err.name == "ValidationError") {
+            message = Object.values(err.errors).map(value => value.message)
+            error = new Error(message)
+        }
+
+        if (err.name == 'CastError') {
+            message = `Resource not found: ${err.path}`;
+            error = new Error(message)
+        }
+
         res.status(err.statusCode).json({
             success: false,
-            message: err.message,
+            message: error.message || 'Internal server error'
+
         })
     }
 }
