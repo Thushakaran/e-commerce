c1354194dc2c6cca7259a5940fef32e99dd53d4b
Thushakaran <thushakarant@gmail.com>
2025-05-25 11:48:13 +0530

    add logout

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -36,3 +36,16 @@ exports.loginUser = catchAsyncError(async (req, res, next) => {
     sendToken(user, 201, res)
 
 })
+
+// logout - /api/v1/logout
+exports.logoutUser = (req, res, next) => {
+    res.cookie('token', null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+        .status(200)
+        .json({
+            success: true,
+            message: "Loggedout"
+        })
+}
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/authController')
+const { registerUser, loginUser, logoutUser } = require('../controllers/authController')
 const router = express.Router();
-const { isAuthenticatedUser } = require('../middlewares/authenticate')
 
-router.route('/register').post(isAuthenticatedUser, registerUser);
+router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
+router.route('/logout').get(logoutUser);
 
 module.exports = router;
