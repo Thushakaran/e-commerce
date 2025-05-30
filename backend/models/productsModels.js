082965e663a89b15cbe21366ddeaa733670c6d08
Thushakaran <thushakarant@gmail.com>
2025-05-30 10:16:02 +0530

    update some thing in product related things

--- backend/models/productsModels.js
+++ backend/models/productsModels.js
@@ -59,7 +59,7 @@ const productSchema = new mongoose.Schema({
         type: Number,
         default: 0
     },
-    review: [
+    reviews: [
         {
             name: {
                 type: String,
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,13 +1,20 @@
 const express = require('express');
-const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/productController');
-const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/authenticate');
+const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct, createReview, getReviews, deleteReview, getAdminProducts } = require('../controllers/productController');
 const router = express.Router();
+const { isAuthenticatedUser, authorizeRoles } = require('../middlewares/authenticate');
 
-router.route('/products').get(isAuthenticatedUser, getProducts);
-router.route('/createProducts').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
+router.route('/products').get(getProducts);
 router.route('/product/:id')
-    .get(getSingleProduct)
-    .put(updateProduct)
-    .delete(deleteProduct)
+    .get(getSingleProduct);
 
-module.exports = router
+
+router.route('/review').put(isAuthenticatedUser, createReview)
+
+//Admin routes
+router.route('/admin/product/new').post(isAuthenticatedUser, authorizeRoles('admin'), newProduct);
+router.route('/admin/products').get(isAuthenticatedUser, authorizeRoles('admin'), getAdminProducts);
+router.route('/admin/product/:id').delete(isAuthenticatedUser, authorizeRoles('admin'), deleteProduct);
+router.route('/admin/product/:id').put(isAuthenticatedUser, authorizeRoles('admin'),  updateProduct);
+router.route('/admin/reviews').get(isAuthenticatedUser, authorizeRoles('admin'), getReviews)
+router.route('/admin/review').delete(isAuthenticatedUser, authorizeRoles('admin'), deleteReview)
+module.exports = router;
